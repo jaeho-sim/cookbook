cf00d9bfd0a82674319c8b5573586bae223da628
Jaeho Sim <jsim@tribalscale.com>
2018-05-29 17:03:52 -0400

    BD/JS [finished:#157856766] - list all food categories in random order when user says 'more'
    Signed-off-by: Brian Dinga <bdinga@tribalscale.com>

--- index.js
+++ index.js
@@ -1,8 +1,180 @@
 'use strict';
+const Alexa = require('ask-sdk');
+var categories = require('./src/category.json').map(category => category.name.value);
 
-var intentHandlers = {};
+// inital 4
+ // Do you want the next 8 of 24 categories or say 'all'?
+  // next | more
+  // prev | last
+  // all
 
-exports.handler = (event, context, callback) => {
+const states = {
+  init: 'INITIAL',
+  category: 'CATEGORY',
+  category_search: 'CATEGORY_SEARCH',
+  pagination: 'PAGINATION'
+}
+const initalYesIntent = (handlerInput) => {
+  const { responseBuilder } = handlerInput;
+  handlerInput.attributesManager.setSessionAttributes({ state: states.category })
+  return responseBuilder
+    .speak('You can ask me to search for recipes within a category such as "breakfast" or "Italian". What category would you like to search or say "more" for more categories?')
+    .reprompt()
+    .getResponse();
+
+};
+const initalNoIntent = ({ responseBuilder }) => {
+  return responseBuilder
+    .speak('What dish are you cooking right now?')
+    .reprompt()
+    .getResponse();
+
+};
+/*
+const FallbackHandler = {
+  canHandle(handlerInput) {
+    const { request } = handlerInput.requestEnvelope;
+    console.log('FALLBACK handlerInput.requestEnvelope: ', handlerInput.requestEnvelope);
+    return request.intent && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+
+    return handlerInput.responseBuilder
+      .speak("I don't understand; I'm out of here")
+      .getResponse();
+  }
+};*/
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log("Inside ErrorHandler - handle");
+    // console.log(`Error handled: ${JSON.stringify(error)}`);
+    console.log('error', error);
+    console.log(`Handler Input: ${JSON.stringify(handlerInput)}`);
+
+    return handlerInput.responseBuilder
+      .speak("Something went wrong")
+      .getResponse();
+  }
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    const { type } = handlerInput.requestEnvelope.request;
+    return ['LaunchRequest'].includes(type);
+  },
+  handle(handlerInput) {
+    handlerInput.attributesManager.setSessionAttributes({ state: states.init })
+    console.log('DEFAULT HANDLE');
+    return handlerInput.responseBuilder
+      .speak("Welcome to cookbook. Would you like recommendations?")
+      .reprompt()
+      .getResponse();
+  }
+};
+
+const CategoryIntentHandler = {
+  canHandle(handlerInput) {
+    const { request } = handlerInput.requestEnvelope;
+    console.log('CATEGORYINTENTHANDLER request: ', request)
+    return request.type === 'IntentRequest' && request.intent.name === 'MoreIntent';
+  },
+  handle(handlerInput) {
+    handlerInput.attributesManager.setSessionAttributes({ state: states.category_search })
+    console.log('CATEGORYINTENTHANDLER HANDLE');
+    return handlerInput.responseBuilder
+      .speak(`Other categories include ${oxfordComma(shuffleArray(categories))}`)
+      .reprompt()
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    console.log("SESSION END?", handlerInput.requestEnvelope.request);
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log("ending session!");
+    return handlerInput.responseBuilder
+      // .speak('Goodbye.')
+      .getResponse();
+  },
+};
+const YesIntent = {
+  canHandle(handlerInput) {
+    const { request } = handlerInput.requestEnvelope;
+    return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.YesIntent';
+  },
+  handle(handlerInput) {
+    const { attributesManager, responseBuilder } = handlerInput;
+
+    const sessionAttributes = attributesManager.getSessionAttributes();
+    if(sessionAttributes.state === states.init) {
+      return initalYesIntent(handlerInput);
+    }
+    return ErrorHandler.handle(handlerInput, new Error('Unhandled Yes'));
+  },
+};
+const NoIntent = {
+  canHandle(handlerInput) {
+    const { request } = handlerInput.requestEnvelope;
+    return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.NoIntent';
+  },
+  handle(handlerInput) {
+    const { attributesManager, responseBuilder } = handlerInput;
+
+    const sessionAttributes = attributesManager.getSessionAttributes();
+    if(sessionAttributes.state === states.init) {
+      return initalNoIntent(handlerInput);
+    }
+    return ErrorHandler.handle(handlerInput, new Error('Unhandled No'));
+  },
+};
+
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(LaunchRequestHandler, CategoryIntentHandler, YesIntent, NoIntent, SessionEndedRequestHandler) /* FallbackHandler, */
+    // .addErrorHandlers(ErrorHandler)
+    .lambda();
+
+const welcomeMessage = 'Welcome to Cookbook. Would you like recommendations?';
+const repromptYnMessage = 'You can say Yes or No.';
+
+
+
+
+const oxfordComma = (strArray, adjoinment = 'or') => {
+  if(!strArray.length) {
+    return '';
+  }
+  let output = '';
+  let prefixArray = strArray.slice(0, strArray.length - 1);
+  return strArray.length > 1 ? `${prefixArray.join(', ')} ${adjoinment} ${strArray.slice(-1)}` : prefixArray[0];
+};
+const shuffleArray = (arr) => {
+    let temp;
+    let index;
+    let ctr = arr.length;
+
+    while (ctr > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * ctr);
+        // Decrease ctr by 1
+        ctr--;
+        // And swap the last element with it
+        temp = arr[ctr];
+        arr[ctr] = arr[index];
+        arr[index] = temp;
+    }
+    return arr;
+}
+
+
+/*
+
+exports.old_handler = (event, context, callback) => {
   try {
     // check application ID
     if (APP_ID !== '' && event.session.application.applicationId !== APP_ID) {
@@ -154,19 +326,19 @@ var getError = (err) => {
 }
 
 
-//--------------------------------------------- Skill specific logic starts here ----------------------------------------- 
+//--------------------------------------------- Skill specific logic starts here -----------------------------------------
 
 //Add your skill application ID from amazon devloper portal
 var APP_ID = '';
 
 var onSessionStarted = (sessionStartedRequest, session) => {
   // add any session init logic here
-    
+
 }
 
 var onSessionEnded = (sessionEndedRequest, session) => {
 // Add any cleanup logic here
-  
+
 }
 
 var onLaunch = (launchRequest, session, response) => {
@@ -221,7 +393,7 @@ intentHandlers['AMAZON.YesIntent'] = (request, session, response, slots) => {
 }
 
 
-
+*/
 
 
 
@@ -229,6 +401,6 @@ intentHandlers['AMAZON.YesIntent'] = (request, session, response, slots) => {
 Example:
 intentHandlers['HelloIntent'] = (request,session,response,slots) => {
   //Intent logic
-  
+
 }
 **/
